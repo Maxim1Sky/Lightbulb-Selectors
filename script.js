f247a83c36e205aa64bf9d0547e484cbc42506ea
Maxim Toma <maxim.toma@sky.uk>
2023-10-19 09:28:43 +0100

    Selector for the output, added count

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Write your code here
+const output = document.querySelector("h3.subtitle");
+let count = 0;
+
+something.evenListener(function () {
+  // your stuff
+
+  count++;
+  output.innerHTML = `You've clicked the lights ${count} times`;
+});
