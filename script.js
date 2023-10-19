7927b824f21d32c7869e4be20568acf632463b26
Maxim Toma <maxim.toma@sky.uk>
2023-10-19 10:56:12 +0100

    Improved version of JS code using 'querySelectorAll'

--- script.js
+++ script.js
@@ -2,27 +2,37 @@
 const output = document.querySelector("h3.subtitle");
 let count = 0;
 
-const bulb1 = document.querySelector("#lightbulb1");
-const bulb2 = document.querySelector("#lightbulb2");
-const bulb3 = document.querySelector("#lightbulb3");
+// const bulb1 = document.querySelector("#lightbulb1");
+// const bulb2 = document.querySelector("#lightbulb2");
+// const bulb3 = document.querySelector("#lightbulb3");
 
-bulb1.addEventListener("click", function () {
-  this.classList.toggle("active");
+// bulb1.addEventListener("click", function () {
+//   this.classList.toggle("active");
 
-  count++;
-  output.innerHTML = `You've clicked the lights ${count} times`;
-});
+//   count++;
+//   output.innerHTML = `You've clicked the lights ${count} times`;
+// });
 
-bulb2.addEventListener("click", function () {
-  this.classList.toggle("active");
+// bulb2.addEventListener("click", function () {
+//   this.classList.toggle("active");
 
-  count++;
-  output.innerHTML = `You've clicked the lights ${count} times`;
-});
+//   count++;
+//   output.innerHTML = `You've clicked the lights ${count} times`;
+// });
 
-bulb3.addEventListener("click", function () {
-  this.classList.toggle("active");
+// bulb3.addEventListener("click", function () {
+//   this.classList.toggle("active");
 
-  count++;
-  output.innerHTML = `You've clicked the lights ${count} times`;
+//   count++;
+//   output.innerHTML = `You've clicked the lights ${count} times`;
+// });
+
+const lightbulbs = document.querySelectorAll(".lightbulb");
+
+lightbulbs.forEach(function (bulb) {
+  bulb.addEventListener("click", function () {
+    this.classList.toggle("active");
+    count++;
+    output.innerHTML = `You've clicked the lights ${count} times`;
+  });
 });
